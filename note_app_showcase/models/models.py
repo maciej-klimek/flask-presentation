ad454cc0485dd83367c04a98a3b02f05fc7fc7c7
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-04-06 17:48:45 +0200

    finished note app

--- note_app_showcase/models/models.py
+++ note_app_showcase/models/models.py
@@ -1,15 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import mapped_column, relationship
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    notes = db.relationship('Note', backref='user', lazy=True)
+    id = mapped_column(db.Integer, primary_key=True)
+    username = mapped_column(db.String(50), unique=True, nullable=False)
+    password = mapped_column(db.String(100), nullable=False)
+    notes = relationship('Note', backref='user', lazy=True)
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = mapped_column(db.Integer, primary_key=True)
+    title = mapped_column(db.String(100), nullable=False)
+    content = mapped_column(db.Text, nullable=False)
+    user_id = mapped_column(db.Integer, db.ForeignKey('user.id'), nullable=False)
